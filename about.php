a4e367a49cce863e336efcc01692fc7fb36209c2
bijay <bijaydas@outlook.com>
2018-01-15 15:38:53 +0530

    Writting about section

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php require '__includes/header.php'; get_header("home"); ?>
+
+<div class="about">
+
+	<div class="wrapper">
+
+		<h5 class="label">About CSS Plato</h5>
+
+		<p>Welcome to CSSPlato. This tool is here to help you save time and money.</p>
+
+		<p>CSSPlato is a, simple and intuitive way of editing and tunning CSS properties. I am a developer and I wanted a new tool to save myself some tedious and repetitive work. This project is free as a gift to our developers community, from which I learn so much.</p>
+
+		<p>This project is in work in progress, there are only three features available, but I'm writing code for next tool and fixing bugs/updating in existing tools.</p>
+
+		<!-- <p>Wanna give me a hand?</p> -->
+
+		<!-- <p>Join me on <a href="https://github.com/bijaydas/cssplato" target="_blank"><i class="fa fa-github icon-github" aria-hidden="true"></i></a></p> -->
+
+		<div class="developers">
+			
+		</div>
+
+
+
+	</div><!-- .wrapper -->
+</div>
+
+<?php require '__includes/footer.php'; ?>
