2a9377c9b85694a72b5f3af18020aa3f3615f434
bijay <bijaydas@outlook.com>
2018-01-16 09:40:26 +0530

    Created contact.php and contact.scss files

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php require '__includes/header.php'; get_header("home"); ?>
+
+<div class="contact">
+
+	<div class="wrapper">
+
+		
+
+	</div><!-- .wrapper -->
+</div>
+
+<?php require '__includes/footer.php'; ?>
